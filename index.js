e3c5096ee11e4030c3dcf495f8231ba28205abc0
BexLedwith <bex.akeakamai@gmail.com>
2021-07-02 11:54:07 -0400

    refactor

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const grid = document.querySelector(".grid");
-const startBtn = document.getElementById("start");
-const score = document.getElementById("score");
+const startButton = document.getElementById("start");
+const scoreDisplay = document.getElementById("score");
 let squares = [];
 let currentSnake = [2, 1, 0];
 let direction = 1;
 const width = 10;
 let appleIndex = 0;
+let score = 0;
+let intervalTime = 1000;
+let speed = 0.9;
 
-// create elements, create 100 elemens, add styling, create array of squares
 function createGrid() {
-  for (let i = width * width; i > 0; i--) {
+  //create 100 of these elements with a for loop
+  for (let i = 0; i < width * width; i++) {
+    //create element
     const square = document.createElement("div");
+    //add styling to the element
     square.classList.add("square");
-    grid.appendChild(square); // must use qs to append child
+    //put the element into our grid
+    grid.appendChild(square);
+    //push it into a new squares array
     squares.push(square);
   }
 }
-
 createGrid();
 
 currentSnake.forEach((index) => squares[index].classList.add("snake"));
 
-// remove last element from current snake array
-// remove styling from last element
-// add square in direction we are heading
-// add styling so we can see it
 function move() {
-  // check if snake hits any walls
   if (
     (currentSnake[0] + width >= width * width && direction === width) || //if snake has hit bottom
     (currentSnake[0] % width === width - 1 && direction === 1) || //if snake has hit right wall
@@ -35,43 +36,71 @@ function move() {
     squares[currentSnake[0] + direction].classList.contains("snake")
   )
     return clearInterval(timerId);
-  const tail = currentSnake.pop();
-  squares[tail].classList.remove("snake");
-  currentSnake.unshift(currentSnake[0] + direction);
 
-  // deal with snake head getting the apple, remove class of apple, grow snake by 1, grow snake array, generate a new apple, add one to the score, speed up our snake
-  // snakeHead ===
+  //remove last element from our currentSnake array
+  const tail = currentSnake.pop();
+  //remove styling from last element
+  squares[tail].classList.remove("snake");
+  //add square in direction we are heading
+  currentSnake.unshift(currentSnake[0] + direction);
+  //add styling so we can see it
+
+  //deal with snake head gets apple
+  if (squares[currentSnake[0]].classList.contains("apple")) {
+    //remove the class of apple
+    squares[currentSnake[0]].classList.remove("apple");
+    //grow our snake by adding class of snake to it
+    squares[tail].classList.add("snake");
+    console.log(tail);
+    //grow our snake array
+    currentSnake.push(tail);
+    console.log(currentSnake);
+    //generate new apple
+    generateApple();
+    //add one to the score
+    score++;
+    //display our score
+    scoreDisplay.textContent = score;
+    //speed up our snake
+    clearInterval(timerId);
+    console.log(intervalTime);
+    intervalTime = intervalTime * speed;
+    console.log(intervalTime);
+    timerId = setInterval(move, intervalTime);
+  }
+
   squares[currentSnake[0]].classList.add("snake");
 }
-
 move();
 
-let timerID = setInterval(move, 1000);
+let timerId = setInterval(move, intervalTime);
 
-function generateApples() {
+function generateApple() {
   do {
     appleIndex = Math.floor(Math.random() * squares.length);
   } while (squares[appleIndex].classList.contains("snake"));
   squares[appleIndex].classList.add("apple");
 }
+generateApple();
 
-generateApples();
-
-//key codes:
-// 39 right arrow, 38 up arrow, 37 left arrow, 40 down arrow
+// 39 is right arrow
+// 38 is for the up arrow
+// 37 is for the left arrow
+// 40 is for the down arrow
 
 function control(e) {
-  e.keyCode === 39
-    ? (direction = 1)
-    : e.keyCode === 38
-    ? (direction = -width)
-    : e.keyCode === 37
-    ? (direction = -1)
-    : e.keyCode === 40
-    ? (direction = +width)
-    : console.log("key press not recognized");
+  if (e.keyCode === 39) {
+    console.log("right pressed");
+    direction = 1;
+  } else if (e.keyCode === 38) {
+    console.log("up pressed");
+    direction = -width;
+  } else if (e.keyCode === 37) {
+    console.log("left pressed");
+    direction = -1;
+  } else if (e.keyCode === 40) {
+    console.log("down pressed");
+    direction = +width;
+  }
 }
-
-// update keyCode is deprecated, switch to e.key
-
-document.addEventListener("keydown", control);
+document.addEventListener("keyup", control);
