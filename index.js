e5e0ba7260aca456fca5a5ec4598143f95f18fb2
BexLedwith <bex.akeakamai@gmail.com>
2021-07-02 09:52:30 -0400

    create squares

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const grid = document.querySelector(".grid");
+const startBtn = document.getElementById("start");
+const score = document.getElementById("score");
+let squares = [];
 
+// create elements, create 100 elemens, add styling, create array of squares
+function createGrid() {
+  for (let i = 100; i > 0; i--) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    grid.appendChild(square); // must use qs to append child
+    squares.push(square);
+  }
+}
+
+createGrid();
